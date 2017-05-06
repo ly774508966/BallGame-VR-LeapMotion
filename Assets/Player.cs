d4c63d149865f4aa581c38fe726c227c5b085888
YukiHirose <db13061@stumail.daido-it.ac.jp>
2017-05-06 23:08:04 +0900

    input initial preparation

---
+++ Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    protected float jump_speed = 5.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.GetComponent<Rigidbody>().velocity = Vector3.up * this.jump_speed;
+        }
+		
+	}
+}
--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # BallGame-VR-LeapMotion
 あれに向けて作っていこうかと
-
-test
-
-tes
